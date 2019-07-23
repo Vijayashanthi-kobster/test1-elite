b628adbf6f3f18bbc2b84ad8718b2e4906c20e67
sivadass <nsivadass@gmail.com>
2019-07-23 18:25:04 +0530

    first commit

---
+++ helloworld/src/helloworld/Basicjava.java
@@ -0,0 +1,17 @@
+package helloworld;
+
+import org.testng.annotations.Test;
+
+public class Basicjava {
+
+	@Test(priority = 1)
+	public void positiveflow() throws Exception {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hi ..this program is for checking whther can run a program in jenkin");
+		
+		
+		
+	}
+
+}
